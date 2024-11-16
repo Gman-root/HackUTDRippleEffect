07f5fa9acd361f8bf782bcb8f347f063c31fbe9b
Pranith Mullapudi <pmullapudi1@gmail.com>
2024-11-16 17:09:05 -0600

    Fix Hydrate flies

--- training data/detection algorithm.py
+++ training data/detection algorithm.py
@@ -9,7 +9,7 @@ def detect_hydrate(file_path):
     df['Hydrate'] = False
     df.loc[((df['Inj Gas Meter Volume Instantaneous'] < .7*df['Inj Gas Meter Volume Setpoint']) |
     (df['Inj Gas Meter Volume Instantaneous'] < .90*df['Inj Gas Meter Volume Setpoint']) & (df['Inj Gas Valve Percent Open'] > 95)),
-    'hydrate']  = True
+    'Hydrate']  = True
     hydrate_flagged_data = file_path.replace("cleaned data", "hydrate flagged data")
     os.makedirs(os.path.dirname(hydrate_flagged_data), exist_ok=True)
     df.to_csv(hydrate_flagged_data, index=False)
