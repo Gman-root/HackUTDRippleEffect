e82a37f6f7a939edbe0e3d9ecf09cbc776398c11
Pranith Mullapudi <pmullapudi1@gmail.com>
2024-11-16 16:37:12 -0600

    Add Hydrate Detection method

---
+++ training data/detection algorithm.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import plotly as pt
+import os
+
+
+
+def detect_hydrate(file_path):
+    df = pd.read_csv(file_path)
+    df['Hydrate'] = False
+    df.loc[((df['Inj Gas Meter Volume Instantaneous'] < .5*df['Inj Gas Meter Volume Setpoint']) & (df['Inj Gas Valve Percent Open'] > 80) |
+    (df['Inj Gas Meter Volume Instantaneous'] < .90*df['Inj Gas Meter Volume Setpoint']) & (df['Inj Gas Valve Percent Open'] > 95)),
+    'hydrate']  = True
+    hydrate_flagged_data = file_path.replace("cleaned data", "hydrate flagged data")
+    os.makedirs(os.path.dirname(hydrate_flagged_data), exist_ok=True)
+    df.to_csv(hydrate_flagged_data, index=False)
+
+def detect_all_hydrate(directory):
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            detect_hydrate(file_path)
+
+
+    
+def main():
+    cleaned_data_directory = os.path.join(os.path.dirname(__file__), "../cleaned data")
+    detect_all_hydrate(cleaned_data_directory)
+
+if __name__ == "__main__":
+    main()
